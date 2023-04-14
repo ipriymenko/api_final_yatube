1f682782034535b9b544d718a695972c82ef6406
Ivan Priymenko <i.priymenko@saticogroup.com>
2023-04-14 22:50:57 +0700

    Initial

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,27 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from .views import PostViewSet, CommentViewSet
+
+app_name = 'api'
+API_VER = 'v1'
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
+    path(f'{API_VER}/', include(router.urls)),
+    path(f'{API_VER}/jwt/create/', TokenObtainPairView.as_view(), name='jwt_create'),
+    path(f'{API_VER}/jwt/refresh/', TokenRefreshView.as_view(), name='jwt_refresh'),
+    path(f'{API_VER}/jwt/verify/', TokenVerifyView.as_view(), name='jwt_verify'),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,30 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.generics import get_object_or_404
+
+from posts.models import Post, Comment
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer, CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly & IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly & IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        return Comment.objects.filter(post=self.kwargs['post_id'])
+
+    def perform_create(self, serializer):
+        serializer.save(
+            post=get_object_or_404(Post, pk=self.kwargs['post_id']),
+            author=self.request.user
+        )
